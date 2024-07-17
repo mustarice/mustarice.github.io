fb46be9d1ad980c6b3fcc6731ee7dfab67cff6cd
mustarice <stasianast45@gmail.com>
2024-07-17 18:07:50 +0700

    Update feeadm.js

--- checkout/qris/feeadm.js
+++ checkout/qris/feeadm.js
@@ -3,14 +3,11 @@ function calculateAdminFee(amount) {
     let nominal = parseInt(amount);
 
     // Calculate the admin fee based on the amount
-    if (nominal > 700000) {
-        // 0.18% for amounts above 700,000
-        return Math.round(nominal * 0.0018);
-    } else if (nominal > 370000) {
-        // 0.5% for amounts above 370,000
-        return Math.round(nominal * 0.005);
+    if (nominal > 250000) {
+        // 0.28% for amounts above 250,000
+        return Math.round(nominal * 0.0028);
     } else {
-        // 0.8% + Rp50 for amounts below 370,000
-        return Math.round(nominal * 0.007 + 20);
+        // 0.4% for amounts below 250,000
+        return Math.round(nominal * 0.004);
     }
 }
